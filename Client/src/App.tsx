d34d63237eae7067e902f2e645d0119645315fdd
Ebi manoj <ebimanoj28@gmail.com>
2026-01-21 21:21:04 +0530

    Cors enabled

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,4 +1,13 @@
+import { useEffect } from 'react';
+import { axiosInstance } from './utils/axios';
+
 function App() {
+  useEffect(() => {
+    (async () => {
+      const res = await axiosInstance.get('/health');
+      console.log(res.data);
+    })();
+  }, []);
   return (
     <div>
       <h1 className="text-red-500">Hello world</h1>
--- Server/app.ts
+++ Server/app.ts
@@ -1,9 +1,20 @@
 import express from 'express';
 import { HttpStatus } from './constants/HttpStatus.js';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
+dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+const allowedOrigins = process.env.CLIENT_URL?.split(',');
+
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
 
 app.get('/health', (req, res) => {
   res.status(HttpStatus.OK).json({
