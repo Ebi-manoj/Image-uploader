3dd7c43a83c42d0be1a72fbff54a03c9a2a7fba2
Ebi manoj <ebimanoj28@gmail.com>
2026-01-25 21:25:01 +0530

    feat:added userprofile in dashboard

--- Client/src/components/Navbar.tsx
+++ Client/src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { ImageUp } from 'lucide-react';
 import { Link } from 'react-router-dom';
 import { cn } from '../lib/utils';
+import ProfileMenu from './ProfileMenu';
 
 export default function Navbar() {
   return (
@@ -9,13 +10,15 @@ export default function Navbar() {
         'sticky top-0 z-50 w-full  bg-white/95 backdrop-blur supports-backdrop-filter:bg-white/60',
       )}
     >
-      <div className="container flex h-14 items-center mx-auto px-4 md:px-8">
+      <div className="container flex h-14 items-center justify-between mx-auto px-4 md:px-8">
         <Link to="/" className="mr-6 flex items-center space-x-2">
           <ImageUp className="h-6 w-6" />
           <span className="hidden font-bold sm:inline-block">
             Image Uploader
           </span>
         </Link>
+        
+        <ProfileMenu />
       </div>
     </nav>
   );
